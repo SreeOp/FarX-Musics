5e43d5327965d135a9f572d278d03c3002b4b20a
SreeOp <134767890+SreeOp@users.noreply.github.com>
2024-08-03 16:38:22 +0530

    support.js

--- commands/support.js
+++ commands/support.js
@@ -41,7 +41,7 @@ module.exports = {
               url: 'https://discord.com/invite/7U6yTNf8Vu'
           })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setImage('https://cdn.discordapp.com/attachments/1266779806582702171/1267772527753826357/096ff227-e675-4307-a969-e2aac7a4c7ba-2.png?ex=66af46ac&is=66adf52c&hm=30e99c9b9d1391414c63c88ed0fdf092a2e1e95f2633fac4940b5e7a228e9502&')
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
